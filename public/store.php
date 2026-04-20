5d7ab91313290d2a8ef14516769a2c08303d6fe6
Érica Pacheco <ericafernandasilva701@gmail.com>
2026-04-19 23:10:47 -0300

    feat: implementado validacoes com mensagem flash

--- public/store.php
+++ public/store.php
@@ -1,13 +1,39 @@
 <?php
-require_once __DIR__ . '/../models/Product.php';
+    require_once __DIR__ . '/../models/Product.php';
+    require_once __DIR__ . '/../config/flash.php';
 
-$data = [
-    'name' => $_POST['name'],
-    'description' => $_POST['description'],
-    'price' => $_POST['price'],
-];
+    session_start();
 
-(new Product())->create($data);
+    $errors = [];
 
-header('Location: index.php');
-exit;
+    $name = trim($_POST['name'] ?? '');
+    $description = trim($_POST['description'] ?? '');
+    $price = $_POST['price'] ?? '';
+
+    // Validações
+    if ($name === '' || strlen($name) < 3) {
+        $errors[] = 'O nome deve ter no mínimo 3 caracteres.';
+    }
+
+    if (!is_numeric($price) || $price <= 0) {
+        $errors[] = 'O preço deve ser um número maior que zero.';
+    }
+
+
+    if (!empty($errors)) {
+        flash('danger', implode('<br>', $errors));
+        $_SESSION['old'] = $_POST;
+        header('Location: create.php');
+        exit;
+    }
+
+
+    (new Product())->create([
+        'name' => $name,
+        'description' => $description,
+        'price' => $price
+    ]);
+
+    flash('success', 'Produto cadastrado com sucesso!');
+    header('Location: index.php');
+    exit;
--- public/update.php
+++ public/update.php
@@ -1,15 +1,42 @@
 <?php
 require_once __DIR__ . '/../models/Product.php';
+require_once __DIR__ . '/../config/flash.php';
 
-$id = $_POST['id'];
+session_start();
 
-$data = [
-    'name' => $_POST['name'],
-    'description' => $_POST['description'],
-    'price' => $_POST['price'],
-];
+$errors = [];
 
-(new Product())->update($id, $data);
+$id = $_POST['id'] ?? null;
+$name = trim($_POST['name'] ?? '');
+$description = trim($_POST['description'] ?? '');
+$price = $_POST['price'] ?? '';
 
+// Validações
+if (!$id || !is_numeric($id)) {
+    $errors[] = 'Produto inválido.';
+}
+
+if ($name === '' || strlen($name) < 3) {
+    $errors[] = 'O NOME deve ter no mínimo 3 caracteres.';
+}
+
+if (!is_numeric($price) || $price <= 0) {
+    $errors[] = 'O PREÇO deve ser um número maior que zero.';
+}
+
+if (!empty($errors)) {
+    flash('danger', implode('<br>', $errors));
+    $_SESSION['old'] = $_POST;
+    header("Location: edit.php?id={$id}");
+    exit;
+}
+
+(new Product())->update($id, [
+    'name' => $name,
+    'description' => $description,
+    'price' => $price
+]);
+
+flash('success', 'Produto atualizado com sucesso!');
 header('Location: index.php');
 exit;
